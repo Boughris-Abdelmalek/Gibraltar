6e0e63a6c783a35e3c8af86bbf9c5c8bbf6262cd
Abdelmalek Boughris <boughris@outlook.fr>
2023-01-13 08:57:11 +0000

    updated nav bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import logo from "../../assets/logo.png";
 import logoSvg from "../../assets/logo.svg";
 import styles from "./header.module.css";
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { SvgIcon } from "@mui/material";
 
 const Header = () => {
@@ -32,14 +33,37 @@ const Header = () => {
             />
           </li>
           <li>
-            <div className={styles.AuthUser}>
-                <p>Hello boughris@outlook.fr</p>
-                <h5>Sign Out</h5>
+            <div className={styles.userContainer}>
+              <p>Hello boughris@outlook.fr</p>
+              <h5>Login</h5>
             </div>
           </li>
-          <li>Orders</li>
-          <li>Prime</li>
-          <li>Basket</li>
+          <li>
+            <div className={styles.userContainer}>
+              <p>Returns</p>
+              <h5>& Orders</h5>
+            </div>
+          </li>
+          <li>
+            <div className={styles.userContainer}>
+              <p>Your</p>
+              <h5>Prime</h5>
+            </div>
+          </li>
+          <li className={styles.basketContainer}>
+          <SvgIcon
+              component={ShoppingBasketIcon}
+              sx={{
+                fill: "white",
+                height: "2.5rem",
+                width: "2.5rem",
+                padding: ".25rem",
+                border: "none",
+                cursor: "pointer",
+              }}
+            />
+            <p>0</p>
+          </li>
         </ul>
       </nav>
     </header>
--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,9 @@ const router = createBrowserRouter([
   },
   {
     path: "/about",
-    element: <About />
-  }
+    element: <About />,
+  },
 ]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

0f66200b5dda9aea46e044485f8d0714d2d068be
Abdelmalek Boughris <boughris@outlook.fr>
2023-03-02 14:11:13 +0000

    Fixed the checkout routing and css

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
+import { useState } from "react";
+
 import getStripe from "../../utils/getStripe";
 
 import useShop from "../../context/ShopContext";
@@ -7,10 +9,13 @@ import ProductInfo from "../../components/productInfos/ProductInfo";
 import styles from "./cart.module.css";
 import Header from "../../components/header/Header";
 
+import { CircularProgress } from "@mui/material";
 import { Button } from "@mui/material";
 
 const Cart = () => {
   const { products, total } = useShop();
+
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -23,15 +28,17 @@ const Cart = () => {
 
     const lineItems = Object.entries(products).map(([id, product]) => ({
       price: product.price_id,
-      quantity: product.quantity
+      quantity: product.quantity,
     }));
+
+    setLoading(true);
 
     const { error } = await stripe.redirectToCheckout({
       lineItems,
       mode: "subscription",
-      successUrl: `http://localhost:5173/success`,
-      cancelUrl: `http://localhost:5173/cancel`,
-      customerEmail: 'customer@email.com',
+      successUrl: `http://localhost:5173/`,
+      cancelUrl: `http://localhost:5173/`,
+      customerEmail: "customer@email.com",
     });
 
     console.warn(error.message);
@@ -40,62 +47,69 @@ const Cart = () => {
   return (
     <>
       <Header />
-      <div className={styles.cartContainer}>
-        <ul className={styles.productContainer}>
-          {Object.entries(products).map((product, index) => {
-            return (
-              <ProductInfo
-                product={product[1]}
-                styles={styles}
-                isCart={true}
-                key={index}
-              />
-            );
-          })}
-        </ul>
-        {total !== 0 ? (
-          <div className={styles.totalContainer}>
-            <h3>Cart Subtotal: {parseFloat(total).toFixed(2)}€</h3>
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: "#F7CA00",
-                borderRadius: "5px",
-                width: "14rem",
-                fontSize: ".7rem",
-                color: "black",
-                "&:hover": {
-                  backgroundColor: "#F7CB09",
-                },
-              }}
-              type="submit"
-              onClick={handleCheckout}
-            >
-              Proceed to checkout
-            </Button>
-          </div>
-        ) : (
-          <div className={styles.alternative}>
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: "#F7CA00",
-                borderRadius: "5px",
-                width: "14rem",
-                fontSize: "1rem",
-                color: "black",
-                "&:hover": {
-                  backgroundColor: "#F7CB09",
-                },
-              }}
-              type="submit"
-              onClick={handleClick}
-            >
-              Shop now !
-            </Button>
-          </div>
-        )}
-      </div>
+      {loading && (
+        <div className={styles.loaderContainer}>
+          <CircularProgress />
+        </div>
+      )}
+      {!loading && (
+        <div className={styles.cartContainer}>
+          <ul className={styles.productContainer}>
+            {Object.entries(products).map((product, index) => {
+              return (
+                <ProductInfo
+                  product={product[1]}
+                  styles={styles}
+                  isCart={true}
+                  key={index}
+                />
+              );
+            })}
+          </ul>
+          {total !== 0 ? (
+            <div className={styles.totalContainer}>
+              <h3>Cart Subtotal: {parseFloat(total).toFixed(2)}€</h3>
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#F7CA00",
+                  borderRadius: "5px",
+                  width: "14rem",
+                  fontSize: ".7rem",
+                  color: "black",
+                  "&:hover": {
+                    backgroundColor: "#F7CB09",
+                  },
+                }}
+                type="submit"
+                onClick={handleCheckout}
+              >
+                Proceed to checkout
+              </Button>
+            </div>
+          ) : (
+            <div className={styles.alternative}>
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#F7CA00",
+                  borderRadius: "5px",
+                  width: "14rem",
+                  fontSize: "1rem",
+                  color: "black",
+                  "&:hover": {
+                    backgroundColor: "#F7CB09",
+                  },
+                }}
+                type="submit"
+                onClick={handleClick}
+              >
+                Shop now !
+              </Button>
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
